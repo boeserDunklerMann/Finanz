d59110c8beb0e55681e7db789c9eb5ee0d9325d7
boeserDunklerMann <me@andre-dunkel.com>
2018-02-11 18:15:48 +0100

    MVVM Framework (Model, ViewModel) completed

---
+++ DA.Finance.Home.MVVM.Model/PlaceAppLogicHere.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DA.Finance.Home.MVVM.Model
+{
+    public class PlaceAppLogicHere
+    {
+		private readonly Func<string, string> _func;
+		public PlaceAppLogicHere(Func<string, string> func) => _func = func;
+
+		public string DoAppLogic(string input) => _func(input);
+	}
+}
---
+++ DA.Finance.Home.MVVM.ViewModel/Presenter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DA.Finance.Home.MVVM.ViewModel
+{
+    public class Presenter : Framework.ObservableObject
+    {
+		private readonly Model.PlaceAppLogicHere _textConverter = new Model.PlaceAppLogicHere(s => s.ToUpper());
+		private string _someText;
+		private readonly ObservableCollection<string> _history = new ObservableCollection<string>();
+
+		public string SomeText
+		{
+			get { return _someText; }
+			set
+			{
+				_someText = value;
+				RaisePropertyChangedEvent(nameof(this.SomeText));
+			}
+		}
+
+		public IEnumerable<string> History => _history;
+
+		public ICommand DoAppLogicCommand
+		{
+			get { return new Framework.DelegateCommand(DoAppLogic); }
+		}
+
+		private void DoAppLogic()
+		{
+			if (string.IsNullOrWhiteSpace(SomeText)) return;
+			AddToHistory(_textConverter.DoAppLogic(SomeText));
+			SomeText = "";
+		}
+
+		private void AddToHistory(string item)
+		{
+			if (!_history.Contains(item))
+				_history.Add(item);
+		}
+	}
+}
