7e1196897196f5664b54a5d450e7eeea51bd6ab3
boeserDunklerMann <me@andre-dunkel.com>
2018-02-11 18:54:44 +0100

    ready before the MVVM View

--- DA.Finance.Home.MVVM.Framework/ObservableObject.cs
+++ DA.Finance.Home.MVVM.Framework/ObservableObject.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace DA.Finance.Home.MVVM.Framework
 {
-    public abstract class ObservableObject : INotifyPropertyChanged
+	public abstract class ObservableObject : INotifyPropertyChanged
     {
 		public event PropertyChangedEventHandler PropertyChanged;
 
