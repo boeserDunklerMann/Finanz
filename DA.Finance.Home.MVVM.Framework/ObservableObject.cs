e726512847b8518b14e2e9a1af8d0a27dbdf5860
boeserDunklerMann <me@andre-dunkel.com>
2018-02-11 18:41:31 +0100

    gitignore

---
+++ DA.Finance.Home.MVVM.Framework/ObservableObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DA.Finance.Home.MVVM.Framework
+{
+    public abstract class ObservableObject : INotifyPropertyChanged
+    {
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void RaisePropertyChangedEvent(string propertyName) => 
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+}
