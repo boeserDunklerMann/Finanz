6f0ea1fcbfc7de3129a2cd28260998c09e270343
boeserDunklerMann <me@andre-dunkel.com>
2018-02-11 17:07:51 +0100

    MVVM.Framework added

---
+++ DA.Finance.Home.MVVM.Framework/DelegateCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DA.Finance.Home.MVVM.Framework
+{
+	/// <summary>
+	/// Implement ICommand, this is what the views actions are bound to.
+	/// </summary>
+	public class DelegateCommand : ICommand
+	{
+		private readonly Action _action;
+
+		public DelegateCommand(Action action) => _action = action;
+
+		public void Execute(object parameter) => _action();
+
+		public bool CanExecute(object parameter) => true;
+		/*
+		 * One thing to note is that CanExecute always returns true. Don’t you just hate it when a button or a menuitem is greyed-out for no apparent reason? Don’t do it! Much better to allow the button to be clicked and then give some informative feedback as to why the intended action cannot be carried out.
+		 * http://www.markwithall.com/programming/2013/03/01/worlds-simplest-csharp-wpf-mvvm-example.html
+		 */
+		public event EventHandler CanExecuteChanged;
+	}
+}
